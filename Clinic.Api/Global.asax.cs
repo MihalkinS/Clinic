d9b6de23825c364536ffb7a775ea799bc8e3212e
MihalkinS <mihalkins.mogilev@gmail.com>
2016-02-27 11:14:12 +0300

    Added AppDbInitializer :   -Added roles in project.   -Added user Admin. Edited db connection.

--- Clinic.Api/Global.asax.cs
+++ Clinic.Api/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Clinic.Api.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -13,6 +15,9 @@ namespace Clinic.Api
     {
         protected void Application_Start()
         {
+
+            Database.SetInitializer<ApplicationDbContext>(new AppDbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
---
+++ Clinic.Api/Models/AppDbInitializer.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace Clinic.Api.Models
+{
+    public class AppDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        //пароль администратора
+        private const string ADMINPASSWORD = "Qwerty6";
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            // Создаем 3 роли (
+            // Администратор
+            // Врач
+            // Зарегистрированный клиент
+            var administratorRole = new IdentityRole("Administrator");
+            var doctorRole = new IdentityRole("Doctor");
+            var clientRole = new IdentityRole("Client");
+
+            //Добавляем в БД
+            roleManager.Create(administratorRole);
+            roleManager.Create(doctorRole);
+            roleManager.Create(clientRole);
+
+            // Добавляем Админа (1 на весь проект)
+            var administrator = new ApplicationUser()
+            {
+                Email = "admin@HealthyPet.com",
+                UserName = "Admin"
+            };
+
+            
+            var result = userManager.Create(administrator, ADMINPASSWORD);
+
+            // Если администратор добавлен, то добавляем ему роль Администратора
+            if (result.Succeeded)
+            {
+                userManager.AddToRole(administrator.Id, administratorRole.Name);
+            }
+
+
+            base.Seed(context);
+        }
+
+    }
+}
