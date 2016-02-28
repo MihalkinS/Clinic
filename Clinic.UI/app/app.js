2b0f5cca5d0be356619b5c6d6a3e64d9bea8fe76
MihalkinS <mihalkins.mogilev@gmail.com>
2016-02-28 11:27:33 +0300

    Added: - file structure (Angular) - lib - Nav

---
+++ Clinic.UI/app/app.js
@@ -0,0 +1,15 @@
+﻿var clinicApp = angular.module('clinicApp', ["ngRoute"])
+    .config(function ($routeProvider) {
+
+        $routeProvider.when('/doctor',
+        {
+            templateUrl: 'doctor.html',
+            controller: 'doctorCtrl'
+        });
+        $routeProvider.when('/main',
+        {
+            templateUrl: 'main.html',
+            controller: 'mainCtrl'
+        });
+        $routeProvider.otherwise({ redirectTo: '/main' });
+    });
