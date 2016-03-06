af8849dcae1778f0ab4da96711a45908344b5d5c
MihalkinS <mihalkins.mogilev@gmail.com>
2016-03-06 10:51:52 +0300

    Edited AuthDbInitializer.cs

--- Clinic.Api/Models/Context/AuthDbInitializer.cs
+++ Clinic.Api/Models/Context/AuthDbInitializer.cs
@@ -8,18 +8,25 @@ namespace Clinic.Api.Models.Context
     public class AuthDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
     {
 
-        // Для заполнения информации об администраторе
-        private struct ADMIN
-        {
-            public const string USERNAME = "Admin";
-            public const string EMAIL = "admin@HealthyPet.com";
-            public const string PASSWORD = "Qwerty6";
-        }
-
         protected override void Seed(ApplicationDbContext context)
         {
+            SetRoles(context);
 
+            SetAdministrator(context);
 
+            base.Seed(context);
+        }
+
+        // Для заполнения информации о ролях
+        private struct ROLES
+        {
+            public const string ADMINISTRATOR = "Administrator";
+            public const string CLIENT = "Client";
+            public const string DOCTOR = "Doctor";
+        }
+
+        private void SetRoles(ApplicationDbContext context)
+        {
             var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
 
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
@@ -37,23 +44,37 @@ namespace Clinic.Api.Models.Context
             roleManager.Create(doctorRole);
             roleManager.Create(clientRole);
 
+        }
+
+        // Для заполнения информации об администраторе
+        private struct ADMIN
+        {
+            public const string USERNAME = "Admin";
+            public const string EMAIL = "admin@HealthyPet.com";
+            public const string PASSWORD = "Qwerty6";
+        }
+
+        private void SetAdministrator(ApplicationDbContext context)
+        {
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
             // Добавляем Админа (1 на весь проект)
             var administrator = new ApplicationUser()
             {
                 Email = ADMIN.EMAIL,
                 UserName = ADMIN.USERNAME
             };
-           
+
             var result = userManager.Create(administrator, ADMIN.PASSWORD);
 
             // Если администратор добавлен, то добавляем ему роль Администратора
             if (result.Succeeded)
             {
-                userManager.AddToRole(administrator.Id, administratorRole.Name);
+                userManager.AddToRole(administrator.Id, ROLES.ADMINISTRATOR);
             }
 
-
-            base.Seed(context);
         }
 
     }
